f102ea1515658592c84e1ec4cad6f5b35626b6e4
cbolles <collinbolles@gmail.com>
2023-09-13 15:32:29 -0400

    Add GraphQL gateway

---
+++ packages/gateway/src/config/configuration.ts
@@ -0,0 +1,11 @@
+export default () => ({
+  signlab: {
+    uri: process.env.SIGNLAB_URI
+  },
+  lex_service: {
+    uri: process.env.LEX_URI
+  },
+  auth: {
+    uri: process.env.AUTH_URI
+  }
+});
