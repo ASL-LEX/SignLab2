9e211aeb794ba01585f3475f1a9e115a113cf00f
cbolles <collinbolles@gmail.com>
2024-01-03 11:55:26 -0500

    Add auth URL to organization

--- packages/server/src/organization/organization.model.ts
+++ packages/server/src/organization/organization.model.ts
@@ -15,6 +15,10 @@ export class Organization {
   /** Maps the `projectId` in the auth service back to the organization */
   @Prop()
   projectId: string;
+
+  @Prop()
+  @Field({ description: 'URL where the user logs in against' })
+  authURL: string;
 }
 
 export type OrganizationDocument = Organization & Document;
