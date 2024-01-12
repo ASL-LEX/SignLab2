20aaafd4a597af7fbc0b88665e772a6b2b9ca651
Akosah <emmanuel.s.akosah.19@gmail.com>
2024-01-12 15:59:10 -0500

    moved delete entry logic to dataset control

--- packages/client/src/components/DatasetTable.component.tsx
+++ packages/client/src/components/DatasetTable.component.tsx
@@ -1,10 +1,8 @@
-import { DataGrid, GridActionsCellItem, GridColDef, GridRowId } from '@mui/x-data-grid';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { useState, useEffect } from 'react';
 import { Dataset, Entry } from '../graphql/graphql';
-import { useDeleteEntryMutation, useEntryForDatasetQuery } from '../graphql/entry/entry';
+import { useEntryForDatasetQuery } from '../graphql/entry/entry';
 import { EntryView } from './EntryView.component';
-import DeleteIcon from '@mui/icons-material/DeleteOutlined';
-import { useConfirmation } from '../context/Confirmation.context';
 
 export interface DatasetTableProps {
   dataset: Dataset;
@@ -24,23 +22,6 @@ export const DatasetTable: React.FC<DatasetTableProps> = (props) => {
       headerName: 'Entry ID',
       width: 150,
       editable: false
-    },
-    {
-      field: 'delete',
-      type: 'actions',
-      headerName: 'Delete',
-      width: 120,
-      maxWidth: 120,
-      cellClassName: 'delete',
-      getActions: (params) => {
-        return [
-          <GridActionsCellItem
-            icon={<DeleteIcon color={'error'} />}
-            label="Delete"
-            onClick={() => handleDelete(params.id)}
-          />
-        ];
-      }
     }
   ];
 
@@ -48,9 +29,6 @@ export const DatasetTable: React.FC<DatasetTableProps> = (props) => {
   const columns = [...defaultColumns, ...(props.additionalColumns ?? [])];
 
   const entryForDatasetResult = useEntryForDatasetQuery({ variables: { dataset: props.dataset._id } });
-  const [deleteEntryMutation] = useDeleteEntryMutation();
-
-  const confirmation = useConfirmation();
 
   // TODO: Add in logic to re-fetch data when the presigned URL expires
   useEffect(() => {
@@ -58,23 +36,6 @@ export const DatasetTable: React.FC<DatasetTableProps> = (props) => {
       setEntries(entryForDatasetResult.data.entryForDataset);
     }
   }, [entryForDatasetResult.data]);
-
-  const handleDelete = async (id: GridRowId) => {
-    // Execute delete mutation
-    confirmation.pushConfirmationRequest({
-      title: 'Delete Entry',
-      message: 'Are you sure you want to delete this project? Doing so will delete all associated tags',
-      onConfirm: async () => {
-        const res = await deleteEntryMutation({ variables: { entry: id.toString() } });
-        if (res.errors) {
-          //TODO show error owith snackbar
-        } else if (res.data) {
-          setEntries([...entries.filter((item) => item._id != id.toString())]);
-        }
-      },
-      onCancel: () => {}
-    });
-  };
 
   return (
     <DataGrid
