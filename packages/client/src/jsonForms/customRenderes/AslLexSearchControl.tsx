5109dd32bbf6b8699102a1328ccb561224fb6102
Akosah <emmanuel.s.akosah.19@gmail.com>
2024-01-24 11:02:49 -0500

    replace search

--- packages/client/src/jsonForms/customRenderes/AslLexSearchControl.tsx
+++ packages/client/src/jsonForms/customRenderes/AslLexSearchControl.tsx
@@ -1,5 +1,7 @@
 import { withJsonFormsControlProps } from '@jsonforms/react';
-import { TextSearch } from '@bu-sail/saas-view';
+import { TextSearch, SearchResults } from '@bu-sail/saas-view';
+import { useState } from 'react';
+import { LexiconEntry } from '../../graphql/graphql';
 
 interface AslLexSearchControlProps {
   data: any;
@@ -11,16 +13,29 @@ function getAslLexCode(aslLexObject: any) {
   return aslLexObject.key ? aslLexObject.key : '';
 }
 
-const AslLexSearchControl = ({ data, handleChange, path }: AslLexSearchControlProps) => (
-  <>
-    <TextSearch
-      width={300}
-      lexicon={data}
-      setSearchResults={(entry) => {
-        handleChange(path, getAslLexCode(entry));
-      }}
-    />
-  </>
-);
+const width = 300;
+const aslLexicon = { _id: '64e4e63ecade2ec090d6765e', name: 'ASL-LEX', schema: {} };
+
+const AslLexSearchControl = ({ handleChange, path }: AslLexSearchControlProps) => {
+  const [searchResults, setSearchResults] = useState<LexiconEntry[]>([]);
+  const [value, setValue] = useState<LexiconEntry | null>(null);
+
+  return (
+    <>
+      <TextSearch width={300} lexicon={aslLexicon} setSearchResults={setSearchResults} />
+      {searchResults.length > 0 && (
+        <SearchResults
+          options={searchResults}
+          value={value}
+          setValue={(entry) => {
+            setValue(entry);
+            handleChange(path, getAslLexCode(entry));
+          }}
+          width={width}
+        />
+      )}
+    </>
+  );
+};
 
 export default withJsonFormsControlProps(AslLexSearchControl);
