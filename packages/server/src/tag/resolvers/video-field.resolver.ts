f63b47ac5afb1222a6d8f8aaa549174a1ceeb64d
cbolles <collinbolles@gmail.com>
2024-01-25 15:17:00 -0500

    Working ability to save tag field

--- packages/server/src/tag/resolvers/video-field.resolver.ts
+++ packages/server/src/tag/resolvers/video-field.resolver.ts
@@ -5,16 +5,22 @@ import { Tag } from '../models/tag.model';
 import { VideoField } from '../models/video-field.model';
 import { TokenContext } from '../../jwt/token.context';
 import { TokenPayload } from '../../jwt/token.dto';
-import { Inject, UnauthorizedException } from '@nestjs/common';
+import { Inject, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { CASBIN_PROVIDER } from '../../permission/casbin.provider';
 import * as casbin from 'casbin';
 import { TagPermissions } from '../../permission/permissions/tag';
+import { JwtAuthGuard } from '../../jwt/jwt.guard';
 
+@UseGuards(JwtAuthGuard)
 @Resolver(() => VideoField)
 export class VideoFieldResolver {
-  constructor(private readonly videoFieldService: VideoFieldService, @Inject(CASBIN_PROVIDER) private readonly enforcer: casbin.Enforcer) {}
+  constructor(
+    private readonly videoFieldService: VideoFieldService,
+    @Inject(CASBIN_PROVIDER) private readonly enforcer: casbin.Enforcer,
+    private readonly tagPipe: TagPipe
+  ) {}
 
-  @Mutation(() => String)
+  @Mutation(() => VideoField)
   async saveVideoField(
     @Args('tag', { type: () => ID }, TagPipe) tag: Tag,
     @Args('field', { type: () => ID }) field: string,
@@ -36,7 +42,11 @@ export class VideoFieldResolver {
 
   @ResolveField(() => String)
   async uploadURL(@Parent() videoField: VideoField, @TokenContext() user: TokenPayload): Promise<string> {
-    if (!(await this.enforcer.enforce(user.id, TagPermissions.CREATE, videoField.tag.toString()))) {
+    const tag = await this.tagPipe.transform(videoField.tag);
+    if (!tag) {
+      throw new Error(`Tag ${videoField.tag} not found`);
+    }
+    if (!(await this.enforcer.enforce(user.id, TagPermissions.CREATE, tag.study.toString()))) {
       throw new UnauthorizedException('User does not have permission to create video fields for this tag');
     }
 
