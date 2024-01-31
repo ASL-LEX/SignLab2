e63ecd560368648fbe2c3d2bbcb69aa3a312885d
Collin Bolles <collinbolles@gmail.com>
2024-01-31 11:19:28 -0500

    feat: Video Recording (Backend) (#36)
    * Creation of video fragments to store incomplete video tags
    * Context for Tag to pass the current tag into JSON Forms fields
    * UI storage of video fragment section
    * UI upload of video to object storage
    * No final saving of incomplete video tags

--- packages/client/src/components/tag/videorecord/VideoRecordField.component.tsx
+++ packages/client/src/components/tag/videorecord/VideoRecordField.component.tsx
@@ -5,6 +5,14 @@ import { withJsonFormsControlProps } from '@jsonforms/react';
 import { StatusProcessCircles } from './StatusCircles.component';
 import { VideoRecordInterface } from './VideoRecordInterface.component';
 import { useEffect, useState, useRef } from 'react';
+import { useApolloClient } from '@apollo/client';
+import {
+  SaveVideoFieldDocument,
+  SaveVideoFieldMutation,
+  SaveVideoFieldMutationVariables
+} from '../../../graphql/tag/tag';
+import { useTag } from '../../../context/Tag.context';
+import axios from 'axios';
 
 const VideoRecordField: React.FC<ControlProps> = (props) => {
   const [maxVideos, setMaxVideos] = useState<number>(0);
@@ -13,8 +21,16 @@ const VideoRecordField: React.FC<ControlProps> = (props) => {
   const [activeIndex, setActiveIndex] = useState<number>(0);
   const [blobs, setBlobs] = useState<(Blob | null)[]>([]);
   const [recording, setRecording] = useState<boolean>(false);
-  const stateRef = useRef<{ validVideos: boolean[]; blobs: (Blob | null)[]; activeIndex: number }>();
-  stateRef.current = { validVideos, blobs, activeIndex };
+  const [videoFragmentID, setVideoFragmentID] = useState<string[]>([]);
+  const stateRef = useRef<{
+    validVideos: boolean[];
+    blobs: (Blob | null)[];
+    activeIndex: number;
+    videoFragmentID: string[];
+  }>();
+  stateRef.current = { validVideos, blobs, activeIndex, videoFragmentID };
+  const client = useApolloClient();
+  const { tag } = useTag();
 
   useEffect(() => {
     if (!props.uischema.options?.minimumRequired) {
@@ -34,6 +50,50 @@ const VideoRecordField: React.FC<ControlProps> = (props) => {
     setBlobs(Array.from({ length: maxVideos }, (_, _i) => null));
   }, [props.uischema]);
 
+  /** Handles saving the video fragment to the database and updating the JSON Forms representation of the data */
+  const saveVideoFragment = async (blob: Blob) => {
+    // Save the video fragment
+    const result = await client.mutate<SaveVideoFieldMutation, SaveVideoFieldMutationVariables>({
+      mutation: SaveVideoFieldDocument,
+      variables: {
+        tag: tag!._id,
+        field: props.path,
+        index: stateRef.current!.activeIndex
+      }
+    });
+
+    if (!result.data?.saveVideoField) {
+      console.error('Failed to save video fragment');
+      return;
+    }
+
+    // Upload the video to the provided URL
+    await axios.put(result.data.saveVideoField.uploadURL, blob, {
+      headers: {
+        'Content-Type': 'video/webm'
+      }
+    });
+
+    // Update the JSON Forms representation of the data to be the ID of the video fragment
+
+    // If the index is longer than the current videoFragmentID array, then add the new ID to the end
+    if (stateRef.current!.activeIndex >= stateRef.current!.videoFragmentID.length) {
+      const updatedVideoFragmentID = [...stateRef.current!.videoFragmentID, result.data!.saveVideoField._id];
+      setVideoFragmentID(updatedVideoFragmentID);
+      props.handleChange(props.path, updatedVideoFragmentID);
+    } else {
+      const updatedVideoFragmentID = stateRef.current!.videoFragmentID.map((id, index) => {
+        if (index === stateRef.current!.activeIndex) {
+          return result.data!.saveVideoField._id;
+        }
+        return id;
+      });
+      setVideoFragmentID(updatedVideoFragmentID);
+      props.handleChange(props.path, updatedVideoFragmentID);
+    }
+  };
+
+  /** Store the blob and check if the video needs to be saved */
   const handleVideoRecord = (video: Blob | null) => {
     const updatedBlobs = stateRef.current!.blobs.map((blob, index) => {
       if (index === stateRef.current!.activeIndex) {
@@ -48,6 +108,9 @@ const VideoRecordField: React.FC<ControlProps> = (props) => {
       return valid;
     });
 
+    if (video !== null) {
+      saveVideoFragment(video);
+    }
     setBlobs(updatedBlobs);
     setValidVideos(updateValidVideos);
   };
