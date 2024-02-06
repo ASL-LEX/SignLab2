1975d5c571251b070687c27730f7d60e9538ed47
cbolles <collinbolles@gmail.com>
2024-02-06 18:11:14 -0500

    bug: Missing key on language selector

--- packages/client/src/components/LanguageSelector.tsx
+++ packages/client/src/components/LanguageSelector.tsx
@@ -25,7 +25,7 @@ export const LanguageSelector: React.FC = () => {
         <InputLabel>{t('components.languageSelector.selectLanguage')}</InputLabel>
         <Select value={language} label="Language" onChange={handleChange}>
           {languages.map((lang) => (
-            <MenuItem value={lang}>{t('languages.' + lang)}</MenuItem>
+            <MenuItem key={lang} value={lang}>{t('languages.' + lang)}</MenuItem>
           ))}
         </Select>
       </FormControl>
