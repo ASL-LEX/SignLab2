1f9871f52cce11e05b3d522a8e92c09f100092e9
cbolles <collinbolles@gmail.com>
2024-03-12 10:13:16 -0400

    Delete training set on study deletion

--- packages/server/src/tag/services/training-set.service.ts
+++ packages/server/src/tag/services/training-set.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { MongooseMiddlewareService } from 'src/shared/service/mongoose-callback.service';
 import { Entry } from '../../entry/models/entry.model';
 import { Study } from '../../study/study.model';
 import { TrainingSet } from '../models/training-set';
 
 @Injectable()
 export class TrainingSetService {
-  constructor(@InjectModel(TrainingSet.name) private readonly trainingSetModel: Model<TrainingSet>) {}
+  constructor(
+    @InjectModel(TrainingSet.name) private readonly trainingSetModel: Model<TrainingSet>,
+    middlewareService: MongooseMiddlewareService
+  ) {
+    middlewareService.register(Study.name, 'deleteOne', async (study: Study) => {
+      await this.removeForStudy(study);
+    });
+  }
 
   async create(study: Study, entries: Entry[]): Promise<TrainingSet> {
     return this.trainingSetModel.create({
@@ -18,5 +26,9 @@ export class TrainingSetService {
 
   async findByStudy(study: Study): Promise<TrainingSet | null> {
     return this.trainingSetModel.findOne({ study: study._id });
+  }
+
+  private async removeForStudy(study: Study): Promise<void> {
+    await this.trainingSetModel.deleteOne({ study: study._id });
   }
 }
