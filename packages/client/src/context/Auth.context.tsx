4a374cd759c55f55645224b8c765a4c8100aad6a
MaolinWei <maolinwei88@gmail.com>
2024-03-22 19:51:25 -0400

    feat: Login/Signup UI Page

--- packages/client/src/context/Auth.context.tsx
+++ packages/client/src/context/Auth.context.tsx
@@ -1,7 +1,6 @@
 import { createContext, FC, useContext, useEffect, useState, ReactNode } from 'react';
 import jwt_decode from 'jwt-decode';
 import { AuthComponent } from '../components/auth/Auth.component';
-import { AuthComponent } from '../components/auth/Auth.component';
 
 export const AUTH_TOKEN_STR = 'token';
 
