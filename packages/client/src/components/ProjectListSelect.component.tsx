4ddc28e85a961ae91e693bba660588c0f2ea83db
cbolles <collinbolles@gmail.com>
2024-04-05 12:03:19 -0400

    Fix issue with check box

--- packages/client/src/components/ProjectListSelect.component.tsx
+++ packages/client/src/components/ProjectListSelect.component.tsx
@@ -9,14 +9,14 @@ import { useSnackbar } from '../context/Snackbar.context';
 
 const ProjectListSelect: React.FC<ControlProps> = (props) => {
   const [projects, setProjects] = useState<Project[]>([]);
-  const [projectIds, setProjectIds] = useState<string[]>([]);
+  const [selectedProjects, setSelectedProjects] = useState<Project[]>([]);
   const getProjects = useGetProjectsQuery();
 
   const { t } = useTranslation();
   const { pushSnackbarMessage } = useSnackbar();
 
-  const handleChange = (event: SelectChangeEvent<typeof projectIds>) => {
-    setProjectIds(typeof event.target.value === 'string' ? event.target.value.split(',') : event.target.value);
+  const handleChange = (event: SelectChangeEvent<typeof selectedProjects>) => {
+    setSelectedProjects(event.target.value as Project[]);
   };
 
   useEffect(() => {
@@ -33,20 +33,20 @@ const ProjectListSelect: React.FC<ControlProps> = (props) => {
       <Select
         multiple
         input={<OutlinedInput label={props.label} />}
-        value={projectIds}
+        value={selectedProjects}
         onChange={handleChange}
         fullWidth
         renderValue={(selected) =>(
           <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
             {selected.map((value) => (
-              <Chip key={value} label={value} />
+              <Chip key={value._id} label={value.name} />
             ))}
           </Box>
         )}
       >
         {projects.map(project => (
-          <MenuItem key={project._id} value={project.name}>
-            <Checkbox checked={projectIds.indexOf(project._id) > -1} />
+          <MenuItem key={project._id} value={project as any}>
+            <Checkbox checked={selectedProjects.indexOf(project) > -1} />
             <ListItemText primary={project.name} />
           </MenuItem>
         ))}
