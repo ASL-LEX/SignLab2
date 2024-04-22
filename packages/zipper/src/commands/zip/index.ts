f9c880fea1f5a32ef392453c9ba00f1a9bf3d07c
cbolles <collinbolles@gmail.com>
2024-04-22 16:28:15 -0400

    Adding in structure for zip request

--- packages/zipper/src/commands/zip/index.ts
+++ packages/zipper/src/commands/zip/index.ts
@@ -6,6 +6,10 @@ export default class ZipHander extends Command {
   static description = 'Untility for zipping entries';
 
   static flags = {
+    target_entries: Flags.file({ exists: true, required: true, description: 'JSON file containing the list of entries to ZIP' }),
+    output_zip: Flags.file({ exists: false, required: true, description: 'Location to output the ZIP' }),
+    notification_webhook: Flags.url({ required: true, description: 'Webhook to call once the ZIP is complete' }),
+    webhook_payload: Flags.file({ exists: true, required: true, description: 'Path to JSON representing payload to send with webhook' }),
     from: Flags.string({ char: 'f', description: 'Who is saying hello', required: true })
   };
 
