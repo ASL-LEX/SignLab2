2329be37321125eb2732156443c62fe414d309e5
cbolles <collinbolles@gmail.com>
2024-04-29 11:52:06 -0400

    Add in dataset select form

--- packages/client/src/pages/datasets/DatasetDownloads.tsx
+++ packages/client/src/pages/datasets/DatasetDownloads.tsx
@@ -1,5 +1,34 @@
+import { useGetDatasetsQuery } from '../../graphql/dataset/dataset';
+import { useEffect, useState } from 'react';
+import { Dataset } from '../../graphql/graphql';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+
+
 export const DatasetDownloads: React.FC = () => {
+  const getDatasetResult = useGetDatasetsQuery();
+
+  const [datasets, setDatasets] = useState<Dataset[]>([]);
+  const [selectedDataset, setSelectedDataset] = useState<Dataset | null>(null);
+
+  useEffect(() => {
+    if (getDatasetResult.data) {
+      setDatasets(getDatasetResult.data.getDatasets);
+    }
+  }, [getDatasetResult.data]);
+
   return (
-    <p>Hello World</p>
+    <>
+      <FormControl>
+        <InputLabel>Dataset Select</InputLabel>
+        <Select
+          value={selectedDataset}
+          onChange={(event) => setSelectedDataset(event.target.value as Dataset)}
+        >
+          {datasets.map(dataset =>
+            <MenuItem value={dataset as any}>{dataset.name}</MenuItem>
+          )}
+        </Select>
+      </FormControl>
+    </>
   );
 };
