8af71d4f202effe623033db8ae8907d059a909bf
cbolles <collinbolles@gmail.com>
2024-05-02 10:57:57 -0400

    Add field for signlab based recordings

--- packages/server/src/download-request/services/study-download-request.service.ts
+++ packages/server/src/download-request/services/study-download-request.service.ts
@@ -3,17 +3,59 @@ import { StudyDownloadRequest } from '../models/study-download-request.model';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateStudyDownloadRequest } from '../dtos/study-download-request-create.dto';
+import { DownloadStatus } from '../models/download-request.model';
+import { Organization } from '../../organization/organization.model';
+import { DownloadRequestService } from './download-request.service';
+import { Entry } from 'src/entry/models/entry.model';
 
 
 @Injectable()
 export class StudyDownloadService {
   constructor(
     @InjectModel(StudyDownloadRequest.name)
-    private readonly downloadRequestModel: Model<StudyDownloadRequest>
+    private readonly downloadRequestModel: Model<StudyDownloadRequest>,
+    private readonly downloadService: DownloadRequestService
   ) {}
 
 
-  async createDownloadRequest(downloadRequest: CreateStudyDownloadRequest) {
+  async createDownloadRequest(downloadRequest: CreateStudyDownloadRequest, organization: Organization): Promise<StudyDownloadRequest> {
+    let request = await this.downloadRequestModel.create({
+      ...downloadRequest,
+      data: new Date(),
+      status: DownloadStatus.IN_PROGRESS,
+      organization: organization._id
+    });
 
+    const bucketLocation = `${this.downloadService.getPrefix()}/${request._id}`;
+
+    // Create the locations for all the artifacts
+    const zipLocation = `${bucketLocation}/entries.zip`;
+    const entryJSONLocation = `${bucketLocation}/entries.json`;
+    const webhookPayloadLocation = `${bucketLocation}/webhook.json`;
+
+    await this.downloadRequestModel.updateOne(
+      { _id: request._id },
+      {
+        $set: {
+          bucketLocation: bucketLocation,
+          entryZIPLocation: zipLocation,
+          entryJSONLocation: entryJSONLocation,
+          webhookPayloadLocation: webhookPayloadLocation
+        }
+      }
+    );
+    request = (await this.downloadRequestModel.findById(request._id))!;
+
+    await this.startZipJob(request);
+
+    return request;
+  }
+
+  private async startZipJob(downloadRequest: StudyDownloadRequest): Promise<void> {
+
+  }
+
+  private async getEntries(downloadRequest: StudyDownloadRequest): Promise<Entry[]> {
+    return [];
   }
 }
