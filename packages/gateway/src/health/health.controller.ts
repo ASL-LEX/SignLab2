43b097dc06d4270060d814d299de4750c2f66c85
cbolles <collinbolles@gmail.com>
2024-06-07 15:27:50 -0400

    Add basic check on signlab endpoint from graphql health check

--- packages/gateway/src/health/health.controller.ts
+++ packages/gateway/src/health/health.controller.ts
@@ -1,13 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import { ConfigService } from '@nestjs/config';
+import { HealthCheck, HealthCheckService, HttpHealthIndicator } from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
-  constructor(private readonly health: HealthCheckService) {}
+  private readonly signlabPingUrl = this.configService.getOrThrow<string>('signlab.uri');
+
+  constructor(
+    private readonly health: HealthCheckService,
+    private readonly configService: ConfigService,
+    private readonly http: HttpHealthIndicator
+  ) {}
 
   @Get()
   @HealthCheck()
   async check() {
-    return this.health.check([]);
+    return this.health.check([
+      // This is a GraphQL endpoint so will return a bad request exception, but that is to be expected
+      () => this.http.responseCheck('signlab', this.signlabPingUrl, (res) => res.status === 400)
+    ]);
   }
 }
--- packages/gateway/src/health/health.module.ts
+++ packages/gateway/src/health/health.module.ts
@@ -1,9 +1,11 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from './health.controller';
 
 @Module({
-  imports: [TerminusModule],
+  imports: [TerminusModule, HttpModule, ConfigModule],
   controllers: [HealthController]
 })
 export class HealthModule {}
