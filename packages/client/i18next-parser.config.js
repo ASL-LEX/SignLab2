f65b7cc56b4d1e09af14c15de1064265a7279ea0
cbolles <collinbolles@gmail.com>
2024-06-26 13:17:40 -0400

    Fix initialization of i18n

--- packages/client/i18next-parser.config.js
+++ packages/client/i18next-parser.config.js
@@ -1,6 +1,6 @@
 export default {
   locales: ['en', 'es'],
-  output: 'public/locales/$LOCALE/translation.json',
+  output: 'public/locales/$LOCALE/$NAMESPACE.json',
   input: ['src/**/*.{ts,tsx}'],
   sort: true,
   createOldCatalogs: true
