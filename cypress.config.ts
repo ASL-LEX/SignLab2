8f135ae113afe3edb0347963d7c877b83629bb07
cbolles <collinbolles@gmail.com>
2024-08-22 14:19:37 -0400

    Add placeholder test

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
