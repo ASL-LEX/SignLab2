9bc828bf61b93773c3825344bba5570821cf3201
Collin Bolles <collinbolles@gmail.com>
2024-10-01 16:21:09 -0400

    chore: Update Translations (#102)
    * Bring in new translations
    * Fix missing translations elements

--- packages/client/src/components/contribute/NoTagNotification.component.tsx
+++ packages/client/src/components/contribute/NoTagNotification.component.tsx
@@ -1,14 +1,17 @@
 import { Stack, Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
 export interface NoTagNotificationProps {
   studyName: string;
 }
 
 export const NoTagNotification: React.FC<NoTagNotificationProps> = ({ studyName }) => {
+  const { t } = useTranslation();
+
   return (
     <Stack direction="column" sx={{ margin: 'auto', maxWidth: 750, alignItems: 'center' }} spacing={2}>
-      <Typography variant="h2">No tags Remaining</Typography>
-      <Typography variant="body1">No tags left for "{studyName}", please check back later</Typography>
+      <Typography variant="h2">{t('tag.noTagsRemaining')}</Typography>
+      <Typography variant="body1">{t('tag.noneLeftExplaination', { studyName })}</Typography>
     </Stack>
   );
 };
