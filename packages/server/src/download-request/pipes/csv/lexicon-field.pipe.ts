f20df10ac14b716e743b51606e92f018fd5f15e5
Collin Bolles <collinbolles@gmail.com>
2025-11-04 10:12:37 -0500

    Add converter for the lexicon field

---
+++ packages/server/src/download-request/pipes/csv/lexicon-field.pipe.ts
@@ -0,0 +1,20 @@
+import { Injectable, PipeTransform } from "@nestjs/common";
+import { CsvFieldTest } from "../../../download-request/types/csv-field";
+
+@Injectable()
+export class LexiconCsvTransformer implements PipeTransform<any, Promise<string>> {
+  async transform(value: any): Promise<string> {
+    if (!value) {
+      return '';
+    }
+
+    return value.key;
+  }
+}
+
+export const lexiconCsvTest: CsvFieldTest = (uischema, _schema) => {
+  if (uischema.options?.customType === 'asl-lex') {
+    return true;
+  }
+  return false;
+}
