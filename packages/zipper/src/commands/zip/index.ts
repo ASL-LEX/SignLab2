f3ffa56dbad7b9e5fc848ab35926f88afa42a3cd
Collin Bolles <collinbolles@gmail.com>
2025-11-20 11:23:47 -0500

    Start replacing previous zip logic

--- packages/zipper/src/commands/zip/index.ts
+++ packages/zipper/src/commands/zip/index.ts
@@ -1,6 +1,8 @@
 import { Command, Flags } from '@oclif/core';
-import AdmZip from 'adm-zip';
 import { readFile } from 'fs/promises';
+import * as archiver from 'archiver';
+import { createReadStream, createWriteStream } from 'fs';
+import { basename } from 'path';
 
 export default class ZipHander extends Command {
   static args = {};
@@ -25,8 +27,15 @@ export default class ZipHander extends Command {
   async run(): Promise<void> {
     const { flags } = await this.parse(ZipHander);
 
-    // Open a ZIP
-    const zip = new AdmZip();
+    // Make the zip file
+    const fileStream = createWriteStream(flags.output_zip);
+
+    // Make a new zipper
+    const archive = archiver.create('zip');
+    archive.pipe(fileStream);
+    archive.on('error', (err) => {
+      console.error(err);
+    });
 
     // Load in the target entries
     const entries: string[] = JSON.parse(await readFile(flags.target_entries, 'utf8'))['entries'];
@@ -37,14 +46,24 @@ export default class ZipHander extends Command {
     // Add the enties to the zip
     for (const entry of entries) {
       try {
-        zip.addLocalFile(entry, 'entries/');
+        // Keep only the file name
+        const entryName = basename(entry);
+
+        // Read in the file
+        const file = createReadStream(entry);
+
+        // Add the file to the archive
+        archive.append(file, { name: entryName });
       } catch (e) {
         console.warn(`Error reading file: ${e}`);
       }
     }
 
-    // Save the zip
-    await zip.writeZipPromise(flags.output_zip);
+    // Wait for all zip operatings to complete
+    await archive.finalize();
+
+    // Close the zip file
+    fileStream.close();
 
     // Post to the webhook to notify of completion
     const result = await fetch(flags.notification_webhook, {
