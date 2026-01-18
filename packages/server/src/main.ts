62f1a3a7e123b452d5b7911f3c918cb06eb6204b
Collin Bolles <collinbolles@gmail.com>
2026-01-18 17:40:38 -0500

    Add global validator for class-validator

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, BadRequestException } from '@nestjs/common';
+import { ValidationError } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  // Class validator global validation handling
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (validationErrors: ValidationError[] = []) => {
+        console.error(JSON.stringify(validationErrors));
+        return new BadRequestException(validationErrors);
+      },
+      transform: true,
+      whitelist: true
+    })
+  );
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- packages/server/src/project/dtos/create.dto.ts
+++ packages/server/src/project/dtos/create.dto.ts
@@ -1,11 +1,14 @@
 import { InputType, OmitType } from '@nestjs/graphql';
+import { IsString } from 'class-validator';
 import { Project } from '../project.model';
 
 @InputType()
 export class ProjectCreate extends OmitType(Project, ['_id', 'created', 'organization'] as const, InputType) {}
 
 export class ProjectCreateRest implements Omit<Project, '_id' | 'created' | 'organization'> {
+  @IsString()
   name: string;
-
+  
+  @IsString()
   description: string;
 }
