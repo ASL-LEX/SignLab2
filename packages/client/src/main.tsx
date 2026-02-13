d1a1fb49e00afa761766fbf14b25f35a386a6ee4
Collin Bolles <collinbolles@gmail.com>
2026-02-13 16:29:01 -0500

    bug: Tables Too Narrow (#126)
    * Issue with permissions tables appearing too narrow
    * Removed top level CSS which wasn't needed

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,6 +1,5 @@
 import * as ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 import { StrictMode } from 'react';
 import './i18n';
 import * as React from 'react';
