acc57345d330c21aee7da0ae31819c6ab4f7e5c3
Collin Bolles <collinbolles@gmail.com>
2026-02-13 10:50:00 -0500

    Start by simplifying state management

--- packages/client/src/components/tag/videorecord/VideoRecordField.component.tsx
+++ packages/client/src/components/tag/videorecord/VideoRecordField.component.tsx
@@ -1,10 +1,10 @@
-import { ControlProps, RankedTester, rankWith } from '@jsonforms/core';
+import { ControlProps, RankedTester, rankWith, ControlElement } from '@jsonforms/core';
 import { ArrowLeft, ArrowRight, ExpandMore } from '@mui/icons-material';
 import { Accordion, AccordionDetails, AccordionSummary, Typography, Stack, Button, IconButton } from '@mui/material';
 import { withJsonFormsControlProps } from '@jsonforms/react';
 import { StatusProcessCircles } from './StatusCircles.component';
 import { VideoRecordInterface } from './VideoRecordInterface.component';
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef, useCallback, FC } from 'react';
 import { useApolloClient } from '@apollo/client';
 import {
   SaveVideoFieldDocument,
@@ -16,8 +16,6 @@ import axios from 'axios';
 import { useTranslation } from 'react-i18next';
 
 const VideoRecordField: React.FC<ControlProps> = (props) => {
-  const [maxVideos, setMaxVideos] = useState<number>(0);
-  const [minimumVideos, setMinimumVideos] = useState<number>(0);
   const [validVideos, setValidVideos] = useState<boolean[]>([]);
   const [activeIndex, setActiveIndex] = useState<number>(0);
   const [blobs, setBlobs] = useState<(Blob | null)[]>([]);
@@ -32,25 +30,13 @@ const VideoRecordField: React.FC<ControlProps> = (props) => {
   stateRef.current = { validVideos, blobs, activeIndex, videoFragmentID };
   const client = useApolloClient();
   const { tag } = useTag();
-  const { t } = useTranslation();
-  const recordingRef = useRef<boolean>(recording);
-  recordingRef.current = recording;
+
+  // Get the max and min number of videos to record
+  const minimumVideos = props.uischema.options!.minimumRequired;
+  const maxVideos = props.uischema.options!.maximumOptional ? props.uischema.options!.maximumOptional : minimumVideos;
 
   const resetState = () => {
-    if (!props.uischema.options?.minimumRequired) {
-      console.error('Minimum number of videos required not specified');
-      return;
-    }
-    const minimumVideos = props.uischema.options.minimumRequired;
-
-    let maxVideos = minimumVideos;
-    if (props.uischema.options?.maximumOptional) {
-      maxVideos = props.uischema.options.maximumOptional;
-    }
-
     setValidVideos(Array.from({ length: maxVideos }, (_, _i) => false));
-    setMinimumVideos(minimumVideos);
-    setMaxVideos(maxVideos);
     setBlobs(Array.from({ length: maxVideos }, (_, _i) => null));
     setActiveIndex(0);
   };
@@ -137,21 +123,21 @@ const VideoRecordField: React.FC<ControlProps> = (props) => {
     setValidVideos(updateValidVideos);
   };
 
-  // Support for using the space key to progress
-  const handleKeyDown = (e: KeyboardEvent) => {
+  const toggleRecording = useCallback((e: KeyboardEvent) => {
     if (e.key == ' ') {
       // Toggle the recording state
-      setRecording(!recordingRef.current);
+      console.log('toggle');
+      setRecording(!recording);
     }
-  };
+  }, [recording]);
 
   useEffect(() => {
     // Listen for spaces
-    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keydown', toggleRecording);
     return () => {
-      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('keydown', toggleRecording);
     };
-  }, []);
+  }, [recording]);
 
   return (
     <Accordion defaultExpanded>
@@ -161,9 +147,8 @@ const VideoRecordField: React.FC<ControlProps> = (props) => {
       </AccordionSummary>
       <AccordionDetails>
         <Stack direction="column" spacing={2} sx={{ width: '50%', margin: 'auto' }}>
-          <Typography variant="h5">
-            {t('tag.videoRequiredOptional', { minimum: minimumVideos, max: maxVideos })}
-          </Typography>
+
+          <VideoCountRequirements min={minimumVideos} max={maxVideos} />
           <StatusProcessCircles isComplete={validVideos} setState={() => {}} activeIndex={activeIndex} />
 
           <Stack direction="row" spacing={2} sx={{ justifyContent: 'center' }}>
@@ -187,14 +172,46 @@ const VideoRecordField: React.FC<ControlProps> = (props) => {
               <ArrowRight fontSize="large" />
             </IconButton>
           </Stack>
-          <Button variant={recording ? 'contained' : 'outlined'} onClick={() => setRecording(!recording)} size="large">
-            {recording ? t('tag.stopRecording') : t('tag.startRecording')}
-          </Button>
+          <RecordButton recording={recording} toggleRecording={() => setRecording(!recording)}/>
         </Stack>
       </AccordionDetails>
     </Accordion>
   );
 };
+
+interface RecordButtonProps {
+  recording: boolean;
+  toggleRecording: () => void;
+}
+
+const RecordButton: FC<RecordButtonProps> = ({ recording, toggleRecording }) => {
+  const { t } = useTranslation();
+
+  return (
+    <Button variant={recording ? 'contained' : 'outlined'} onClick={toggleRecording} size="large">
+      {recording ? t('tag.stopRecording') : t('tag.startRecording')}
+    </Button>
+  );
+};
+
+interface VideoCountRequirementsProps {
+  max: number;
+  min: number;
+}
+
+const VideoCountRequirements: FC<VideoCountRequirementsProps> = ({ max, min }) => {
+  const { t } = useTranslation();
+
+  if (!min)  {
+    console.error('Minimum number of videos not found');
+  }
+
+  return (
+    <Typography variant="h5">
+      {t('tag.videoRequiredOptional', { minimum: min, max: max  })}
+    </Typography>
+  );
+}
 
 export const videoFieldTester: RankedTester = rankWith(10, (uischema, _schema, _rootSchema) => {
   return uischema.options != undefined && uischema.options && uischema.options.customType === 'video';
